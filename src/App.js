457271436884d13a37b803899fb7b1ebe0707c41
oswelR1 <heshanoswel@gmail.com>
2024-09-28 13:37:20 +0530

    Fixed Vercel deployment failing 2

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
-import React, { useContext } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { AuthProvider, AuthContext } from './context/AuthContext';
-import ContentManagementUI from './components/ContentManagementUI';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
-
-const ProtectedRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
-  if (!user) {
-    return <Navigate to="/login" replace />;
-  }
-  return children;
-};
-
-function AppContent() {
-  return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/" element={
-        <ProtectedRoute>
-          <ContentManagementUI />
-        </ProtectedRoute>
-      } />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  );
-}
+import ContentManagementUI from './components/ContentManagementUI';
+import ErrorBoundary from './components/ErrorBoundary';
 
 function App() {
   return (
-    <AuthProvider>
+    <ErrorBoundary>
       <Router>
-        <div className="App">
-          <AppContent />
-        </div>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<ContentManagementUI />} />
+        </Routes>
       </Router>
-    </AuthProvider>
+    </ErrorBoundary>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // Make sure this line is present
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
